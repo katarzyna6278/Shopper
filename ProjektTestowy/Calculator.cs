7c96e9b5512ef41c25ae2862a9d24dcb031b6a3b
Katarzyna Kasprzak <katarzyna_kasprzak@next.co.uk>
2024-04-20 12:09:24 +0100

    nowa klasa calculator

---
+++ ProjektTestowy/Calculator.cs
@@ -0,0 +1,14 @@
+﻿namespace ProjektTestowy
+{
+    public class Calculator
+    {
+        public int FirstNumber { get; set; }
+        public int SecondNumber { get; set; }
+
+
+        public int Add()
+        {
+            return FirstNumber + SecondNumber;
+        }
+    }
+}
