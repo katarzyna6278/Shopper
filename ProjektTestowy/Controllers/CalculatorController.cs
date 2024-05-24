ade8346821c65184701e000ccb9fb83b386da683
Gosia Jaglarz <106768948+gosiajaglarz@users.noreply.github.com>
2024-05-24 20:01:47 +0200

    commit CalculatorController

---
+++ ProjektTestowy/Controllers/CalculatorController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjektTestowy.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CalculatorController : ControllerBase
+    {
+        private readonly Calculator _calculator;
+
+        public CalculatorController()
+        {
+            _calculator = new Calculator();
+        }
+        public int Add(int firstNumer, int secondNumber)
+        {
+            _calculator.FirstNumber = firstNumer;
+            _calculator.SecondNumber = secondNumber;
+
+            return _calculator.Add();
+        }
+    }
+}
