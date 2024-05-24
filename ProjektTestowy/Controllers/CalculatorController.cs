3ce3ecfd25bf658f4255927bab0acc96e7a31c2a
Wiola <wioleta.piekarus@gmail.com>
2024-05-24 19:56:21 +0200

    24/05

---
+++ ProjektTestowy/Controllers/CalculatorController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ProjektTestowy.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CalculatorController : ControllerBase
+    {
+        private readonly Calculator _calculator;
+        public CalculatorController()
+        {
+            _calculator = new Calculator();
+        }
+
+        [HttpGet("/Calculator/Add")]
+        public int Add(int firstNumber, int secondNumber)
+        { 
+            _calculator.FirstNumber = firstNumber;
+            _calculator.SecondNumber = secondNumber;
+
+            return _calculator.Add();
+        }
+    }
+}
