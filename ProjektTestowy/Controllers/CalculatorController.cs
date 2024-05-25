14d8b230157e88d96ce0614096faddc66d7a4dfe
Gosia Jaglarz <106768948+gosiajaglarz@users.noreply.github.com>
2024-05-25 13:48:35 +0200

    API

--- ProjektTestowy/Controllers/CalculatorController.cs
+++ ProjektTestowy/Controllers/CalculatorController.cs
@@ -7,22 +7,42 @@ using System.Threading.Tasks;
 
 namespace ProjektTestowy.Controllers
 {
+    //atrybuty controllera
     [ApiController]
     [Route("[controller]")]
     public class CalculatorController : ControllerBase
     {
         private readonly Calculator _calculator;
 
+
+        //konstruktor
         public CalculatorController()
         {
             _calculator = new Calculator();
         }
+
+        //endpoint
+        [HttpGet("/Calculator/Add")]
         public int Add(int firstNumer, int secondNumber)
         {
             _calculator.FirstNumber = firstNumer;
             _calculator.SecondNumber = secondNumber;
 
-            return _calculator.Add();
+            var result = _calculator.Add();
+
+            return result;
+        }
+        //endpoint do odejmowania
+        [HttpGet("/Calculator/Substract")]
+
+        public int Substract(int firstNumber, int secondNumber)
+        {
+            _calculator.FirstNumber = firstNumber;
+            _calculator.SecondNumber = secondNumber;
+
+            var result = _calculator.Subtract();
+
+            return result;
         }
     }
 }
